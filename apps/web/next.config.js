9fdc535b2b61a1a5ab4d4294fd937b804f4e8a38
dondendo89 <Domenico.buttafarro@gmail.com>
2025-09-01 10:10:41 +0200

    fix: Remove deprecated appDir option from next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   transpilePackages: ['@qa-playwright/shared', '@qa-playwright/prisma'],
 }
 
